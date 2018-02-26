d0acd453b8e263fa7398a7b9ef5e555a4ab4a985
Tech-A10 <tech-a10@MacBook-Air-8.local>
2018-02-26 11:04:22 +0000

    slight modifications

--- js/index.js
+++ js/index.js
@@ -1,5 +1,32 @@
 
 
+var game = document.getElementById("container");
+game.width = 500;
+game.height = 400;
+
+var jumper = document.getElementById("Square");
+
+
+
+/*
+var platform = document.getElementById("Platform");
+
+var gravity = 2;
+var yMax = 0;
+var yCurrent = 0;
+var currentPoint = 0;
+
+*/ 
+
+
+
+
+
+
+
+
+
+
 
 
 
