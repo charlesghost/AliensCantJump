3b2810361b39c2365a92ceec8fc34fb86b210ad1
Tech-A10 <tech-a10@MacBook-Air-8.local>
2018-02-27 14:50:38 +0000

    fixed functions

--- js/index.js
+++ js/index.js
@@ -2,42 +2,76 @@
 // Create Player + Controls
 // Create multiple platorms with possible collision
 
+var gravity = 1;
+var maxheight;
+var timer;
+var jump = 32;
+var velocity = 0;
+var SCREEN_WIDTH = 400;
+var SCREEN_HEIGHT = 500;
 
+maxheight = Math.round($("#wrapper").height() - $("#player").height());
 
+timer = setInterval(update, 1000/60);
 
 var Player = document.getElementById("player");
+
+
+// Keypad 
 
 $(document).keydown(function(e) {
     switch (e.which) {
     case 37:
         $('#player').stop().animate({
-            left: '-=10'
+            left: '-=30'
         }); //left arrow key
         break;
     case 38:
         $('#player').stop().animate({
-            top: '-=10'
+            top: '-=30'
         }); //up arrow key
         break;
     case 39:
         $('#player').stop().animate({
-            left: '+=10'
+            left: '+=30'
         }); //right arrow key
         break;
     case 40:
         $('#player').stop().animate({
-            top: '+=10'
+            top: '+=30'
         }); //bottom arrow key
         break;
     }
 })
 
+function update() {
+	if (velocity === 0 && $("#player").position().top === maxheight) {
+	} else if (velocity > 0 && $("#player").position().top >= maxheight) {
+		$("#player").css("top", maxheight + "px");
+		velocity = 0;
+	} else {
+		velocity += gravity;
+		var newPos = $("#player").position().top + velocity + "px";
+		$("#player").css("top", newPos);
+		velocity = 3;
+
+	}
+};
+
+function collision(){
+
+	
+
+
+}
+
+
+
+
 var game = function(_gameDiv) {
 
 	var that = this;
-	var interval = 20;
-	var setTimeInterval;
-
+	
 	var SCREEN_WIDTH = 400;
     var SCREEN_HEIGHT = 500;
 
